14386289d994175f155ede868fea87703660e5e3
Carlos Adrián Morales <morales@carlosadrian.mx>
2020-12-08 11:02:49 -0600

    Appends a label with size to elements matched by a CSS selector, defaults to immediate children of `.sizing`.  * Default labels contains width and height and are floated to the right.  * Responsive
    This commit adds zIndex flag, styling and images to README.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# sizing
-Sizing appends a label with block dimensions, width & height, to elements matched by a selector.
+# Blocks Sizing
+
+Appends a label with size to elements matched by a CSS selector, defaults to immediate children of `.sizing`.
+ * Default labels contains width and height and are floated to the right.
+ * Responsive 
+
+Sample on big screen
+
+![Sample on big screen](images/big-screen.png) 
+
+Sample on small screen
+
+![Sample on small screen](images/small-screen.png)
+
+Default with `zIndex` parameter as `true`
+
+![Sample with z-index](images/z-index.png)
+
+## Installation
+
+Just add `sizing.js` to your project, it will match by default all parent containers with `.sizing` class.
+
+You can provide another CSS selector using the `Sizing(<selector>)` initialiser.
+
+Optionally, add `sizing.css` stylesheet, it sets default styling for size labels.
+
+## Usage
+The Sizing function has the following signature
+ 
+`Sizing(<CSS selector>, [zIndex])`
+
+Where
+
+`<CSS selector>` **string**: any CSS selector
+
+`zIndex` **boolean** *optional*: adds `z-index` value to the labels.
+
+Thank's.
+  
--- sizing.js
+++ sizing.js
@@ -1,21 +1,50 @@
 /**
- * Sizing plugin
- * Gets dimensions of immediate elements and appends it
+ * Blocks Sizing
+ * Appends a label to all elements matched by a CSS selector, defaults to immediate children of `.sizing`. Default label contains width and height.
+ * Labels are floated to the right
+ * @version 0.1a
  */
-let Sizing = function(selector) {
+
+/**
+ *
+ * @param selector string CSS selector for the elements to append the size label
+ * @param zIndex boolean `true` adds the z-index of the element if is declared
+ * @constructor
+ */
+let Sizing = function(selector, zIndex) {
+
     (function(win, doc, sel) {
         let els = doc.querySelectorAll(sel),
             tag;
 
-        els.forEach(function(el, i) {
-            tag = doc.createElement('mark');
-            tag.classList.add('sizing-dimension');
-            tag.textContent = el.clientWidth + 'x' + el.clientHeight;
-            el.prepend(tag);
-        });
+        function init() {
+            // Cleanup if any
+            doc.querySelectorAll('.sizing-dimension').forEach(function(el, key) {
+                el.remove();
+            });
+
+            els.forEach(function(el, i) {
+                tag = doc.createElement('mark');
+
+                tag.classList.add('sizing-dimension');
+                tag.textContent = el.clientWidth + 'x' + el.clientHeight;
+
+                if(zIndex === true) {
+                    tag.textContent += ' z:' + win.getComputedStyle(el).zIndex;
+                }
+
+                el.prepend(tag);
+                el.classList.add('.sizing-element')
+
+            });
+        }
+
+        win.addEventListener('load', init, false);
+        win.addEventListener('resize', init, false);
+
     })(window, document, selector);
-}
+};
 
 // A default initialisation
-if(document.querySelector('.sizing'))
+if(document.querySelectorAll('.sizing').length > 0)
     Sizing('.sizing > *');
